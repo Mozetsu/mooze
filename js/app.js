6c4228012f542f993ed5372d93fd9c62d7d470cb
Mozetsu <code.moz.00@gmail.com>
2020-07-06 01:34:56 +0100

    Submit UI #3

--- js/app.js
+++ js/app.js
@@ -2,3 +2,14 @@
 particlesJS.load('particles-js', './particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
+
+var qrcode = new QRCode(document.getElementById('qrcode'), {
+	text: 'Mozetsu',
+	width: 295,
+	height: 295,
+	colorDark: '#343233',
+	colorLight: '#F5DEBA',
+	correctLevel: QRCode.CorrectLevel.H,
+});
+
+document.querySelector('.app').style.filter = 'blur(4px)';
